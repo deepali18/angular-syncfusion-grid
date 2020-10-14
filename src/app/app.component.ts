94e5b9b7ddf6d7fc5b1b468d854b41db404fe1e0
deepali18 <deepali.imagine@gmail.com>
2020-10-14 18:02:24 +0530

    5 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { data } from './datasource';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+
+export class AppComponent implements OnInit {
+
+    name = 'Angular ' + VERSION.major;
+    public data: object[];
+
+    public pageSettings: PageSettingsModel;
+    ngOnInit(): void {
+        this.data = data;
+        this.pageSettings = { pageSize: 6 };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+// import the GridModule for the Grid component
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+
+import { PageService, SortService, FilterService, GroupService } from '@syncfusion/ej2-angular-grids';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, GridModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [PageService,
+                SortService,
+                FilterService,
+                GroupService]
 })
 export class AppModule { }
